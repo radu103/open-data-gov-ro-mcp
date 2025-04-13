8a9c35cab3a8b44753d211c8559bd74eb0dc9bb0
Radu Simen <radu103@hotmail.com>
2025-04-13 23:56:29 +0300

    sln file and mcp tool server as sse

--- Tools/DataGovRoUrls.cs
+++ Tools/DataGovRoUrls.cs
@@ -7,6 +7,9 @@ namespace OpenDataGovRo.Tools
         {
             new KeyValuePair<string, string>("firme_neradiate", "https://data.gov.ro/dataset/11b4e62a-daa9-4093-b9a9-5c5ca259a096/resource/2cb08cf2-258d-4334-b572-0ea33e04d62a/download/3firme_neradiate_cu_sediu_18-03-2025.csv"),
             new KeyValuePair<string, string>("firme_radiate", "https://data.gov.ro/dataset/11b4e62a-daa9-4093-b9a9-5c5ca259a096/resource/2cb08cf2-258d-4334-b572-0ea33e04d62a/download/3firme_radiate_cu_sediu_18-03-2025.csv"),
+            new KeyValuePair<string, string>("siruta_uat", "http://data.gov.ro/storage/f/2013-11-01T11%3A49%3A59.808Z/siruta.csv"),
+            new KeyValuePair<string, string>("siruta_judete", "http://data.gov.ro/storage/f/2013-11-01T11%3A53%3A13.359Z/siruta-judete.csv"),
+            new KeyValuePair<string, string>("siruta_zone", "http://data.gov.ro/storage/f/2013-11-01T11%3A52%3A07.473Z/siruta-zone.csv")
         };
     }
 }
